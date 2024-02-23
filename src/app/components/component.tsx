901155882df861a7e27d01b155304436b183f6b2
Andres Solano <andres@wikirealty.com>
2024-02-23 16:38:40 -0600

    initial layout

---
+++ src/app/components/component.tsx
@@ -0,0 +1,13 @@
+export default function Component() {
+  return (
+    <div className="py-2 px-4 flex flex-col items-center">
+      <span>Component1</span>
+      <button type="button" className="text-black py-2 px-4 rounded">
+        Prev
+      </button>
+      <button type="button" className="text-black py-2 px-4 rounded">
+        Next
+      </button>
+    </div>
+  );
+}
